2ddbc4538dadcfff84dd635727026ba07a39d107
17520720 <50265171+17520720@users.noreply.github.com>
2019-11-27 21:56:39 +0700

    Add files via upload

---
+++ SplitImage/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace SplitImage
+{
+    partial class form_SplitImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(form_SplitImage));
+            this.bt_ChooseFile = new System.Windows.Forms.Button();
+            this.tb__CellSize = new System.Windows.Forms.TextBox();
+            this.lb_CellSize = new System.Windows.Forms.Label();
+            this.tb_numRows = new System.Windows.Forms.TextBox();
+            this.lb_soHang = new System.Windows.Forms.Label();
+            this.lb_textDuongdan = new System.Windows.Forms.Label();
+            this.tb_Folder = new System.Windows.Forms.TextBox();
+            this.gb_Infor = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_FileName = new System.Windows.Forms.TextBox();
+            this.bt_ChooseFolder = new System.Windows.Forms.Button();
+            this.bt_Export = new System.Windows.Forms.Button();
+            this.lb_caution = new System.Windows.Forms.Label();
+            this.gb_Infor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bt_ChooseFile
+            // 
+            this.bt_ChooseFile.Location = new System.Drawing.Point(317, 42);
+            this.bt_ChooseFile.Name = "bt_ChooseFile";
+            this.bt_ChooseFile.Size = new System.Drawing.Size(75, 23);
+            this.bt_ChooseFile.TabIndex = 0;
+            this.bt_ChooseFile.Text = "Chọn File";
+            this.bt_ChooseFile.UseVisualStyleBackColor = true;
+            this.bt_ChooseFile.Click += new System.EventHandler(this.bt_ChooseFile_Click);
+            // 
+            // tb__CellSize
+            // 
+            this.tb__CellSize.Location = new System.Drawing.Point(186, 142);
+            this.tb__CellSize.Name = "tb__CellSize";
+            this.tb__CellSize.Size = new System.Drawing.Size(75, 20);
+            this.tb__CellSize.TabIndex = 3;
+            this.tb__CellSize.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb__CellSize_KeyPress);
+            // 
+            // lb_CellSize
+            // 
+            this.lb_CellSize.AutoSize = true;
+            this.lb_CellSize.ForeColor = System.Drawing.Color.White;
+            this.lb_CellSize.Location = new System.Drawing.Point(71, 145);
+            this.lb_CellSize.Name = "lb_CellSize";
+            this.lb_CellSize.Size = new System.Drawing.Size(80, 13);
+            this.lb_CellSize.TabIndex = 3;
+            this.lb_CellSize.Text = "Kích thước Cell";
+            // 
+            // tb_numRows
+            // 
+            this.tb_numRows.Location = new System.Drawing.Point(186, 168);
+            this.tb_numRows.Name = "tb_numRows";
+            this.tb_numRows.Size = new System.Drawing.Size(75, 20);
+            this.tb_numRows.TabIndex = 4;
+            this.tb_numRows.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tb_numRows_KeyPress);
+            // 
+            // lb_soHang
+            // 
+            this.lb_soHang.AutoSize = true;
+            this.lb_soHang.ForeColor = System.Drawing.Color.White;
+            this.lb_soHang.Location = new System.Drawing.Point(71, 171);
+            this.lb_soHang.Name = "lb_soHang";
+            this.lb_soHang.Size = new System.Drawing.Size(90, 13);
+            this.lb_soHang.TabIndex = 5;
+            this.lb_soHang.Text = "Số hàng của Grid";
+            // 
+            // lb_textDuongdan
+            // 
+            this.lb_textDuongdan.AutoSize = true;
+            this.lb_textDuongdan.ForeColor = System.Drawing.Color.White;
+            this.lb_textDuongdan.Location = new System.Drawing.Point(71, 88);
+            this.lb_textDuongdan.Name = "lb_textDuongdan";
+            this.lb_textDuongdan.Size = new System.Drawing.Size(99, 13);
+            this.lb_textDuongdan.TabIndex = 6;
+            this.lb_textDuongdan.Text = "Đường dẫn xuất file";
+            // 
+            // tb_Folder
+            // 
+            this.tb_Folder.Location = new System.Drawing.Point(6, 104);
+            this.tb_Folder.Name = "tb_Folder";
+            this.tb_Folder.ReadOnly = true;
+            this.tb_Folder.Size = new System.Drawing.Size(297, 20);
+            this.tb_Folder.TabIndex = 2;
+            // 
+            // gb_Infor
+            // 
+            this.gb_Infor.Controls.Add(this.label1);
+            this.gb_Infor.Controls.Add(this.tb_FileName);
+            this.gb_Infor.Controls.Add(this.tb_Folder);
+            this.gb_Infor.Controls.Add(this.bt_ChooseFolder);
+            this.gb_Infor.Controls.Add(this.lb_textDuongdan);
+            this.gb_Infor.Controls.Add(this.lb_soHang);
+            this.gb_Infor.Controls.Add(this.tb_numRows);
+            this.gb_Infor.Controls.Add(this.lb_CellSize);
+            this.gb_Infor.Controls.Add(this.tb__CellSize);
+            this.gb_Infor.Controls.Add(this.bt_ChooseFile);
+            this.gb_Infor.ForeColor = System.Drawing.Color.Black;
+            this.gb_Infor.Location = new System.Drawing.Point(205, 103);
+            this.gb_Infor.Name = "gb_Infor";
+            this.gb_Infor.Size = new System.Drawing.Size(398, 214);
+            this.gb_Infor.TabIndex = 9;
+            this.gb_Infor.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(71, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "File ảnh cần cắt";
+            // 
+            // tb_FileName
+            // 
+            this.tb_FileName.Location = new System.Drawing.Point(6, 44);
+            this.tb_FileName.Name = "tb_FileName";
+            this.tb_FileName.ReadOnly = true;
+            this.tb_FileName.Size = new System.Drawing.Size(297, 20);
+            this.tb_FileName.TabIndex = 7;
+            // 
+            // bt_ChooseFolder
+            // 
+            this.bt_ChooseFolder.Location = new System.Drawing.Point(317, 102);
+            this.bt_ChooseFolder.Name = "bt_ChooseFolder";
+            this.bt_ChooseFolder.Size = new System.Drawing.Size(75, 23);
+            this.bt_ChooseFolder.TabIndex = 1;
+            this.bt_ChooseFolder.Text = "Duyệt";
+            this.bt_ChooseFolder.UseVisualStyleBackColor = true;
+            this.bt_ChooseFolder.Click += new System.EventHandler(this.bt_ChooseFolder_Click);
+            // 
+            // bt_Export
+            // 
+            this.bt_Export.Location = new System.Drawing.Point(378, 306);
+            this.bt_Export.Name = "bt_Export";
+            this.bt_Export.Size = new System.Drawing.Size(75, 23);
+            this.bt_Export.TabIndex = 5;
+            this.bt_Export.Text = "Xuất file";
+            this.bt_Export.UseVisualStyleBackColor = true;
+            this.bt_Export.Click += new System.EventHandler(this.bt_Export_Click);
+            // 
+            // lb_caution
+            // 
+            this.lb_caution.AutoSize = true;
+            this.lb_caution.ForeColor = System.Drawing.Color.DarkRed;
+            this.lb_caution.Location = new System.Drawing.Point(12, 428);
+            this.lb_caution.Name = "lb_caution";
+            this.lb_caution.Size = new System.Drawing.Size(0, 13);
+            this.lb_caution.TabIndex = 10;
+            // 
+            // form_SplitImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lb_caution);
+            this.Controls.Add(this.bt_Export);
+            this.Controls.Add(this.gb_Infor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "form_SplitImage";
+            this.Text = "Not Responding...";
+            this.Load += new System.EventHandler(this.form_SplitImage_Load);
+            this.gb_Infor.ResumeLayout(false);
+            this.gb_Infor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bt_ChooseFile;
+        private System.Windows.Forms.TextBox tb__CellSize;
+        private System.Windows.Forms.Label lb_CellSize;
+        private System.Windows.Forms.TextBox tb_numRows;
+        private System.Windows.Forms.Label lb_soHang;
+        private System.Windows.Forms.Label lb_textDuongdan;
+        private System.Windows.Forms.TextBox tb_Folder;
+        private System.Windows.Forms.GroupBox gb_Infor;
+        private System.Windows.Forms.Button bt_Export;
+        private System.Windows.Forms.Button bt_ChooseFolder;
+        private System.Windows.Forms.TextBox tb_FileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_caution;
+    }
+}
+
---
+++ SplitImage/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitImage
+{
+    struct Cell {
+        public int ID;
+        public int row, column;
+        public int count;
+        public Bitmap cell_Bitmap;
+    }
+    public partial class form_SplitImage : Form
+    {
+        private string str_ImageSources;
+        private string str_FolderDes;
+
+        public form_SplitImage()
+        {
+            InitializeComponent();
+ 
+        }
+
+        private void form_SplitImage_Load(object sender, EventArgs e)
+        {
+            gb_Infor.Location = new Point(this.Width / 2 - gb_Infor.Width / 2, this.Height / 2 - gb_Infor.Height / 2 - 20);
+            bt_Export.Location = new Point(gb_Infor.Location.X + gb_Infor.Width / 2 - bt_Export.Width / 2, gb_Infor.Location.Y + gb_Infor.Height - bt_Export.Height / 2);
+            tb__CellSize.ShortcutsEnabled = false;
+            tb_numRows.ShortcutsEnabled = false;
+
+            playMusicBg();
+        }
+
+        private void tb__CellSize_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void tb_numRows_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void bt_ChooseFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files (*.png, *.jpeg, *.bmp) |*.png; *.jpeg; *.bmp";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.Multiselect = false;
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                tb_FileName.Text = openFileDialog.SafeFileName;
+                str_ImageSources = openFileDialog.FileName;
+            }
+        }
+
+        private string GetFilePath(string stringFile)
+        {
+            string filePath = stringFile.Replace("\\", "\\\\");
+            //MessageBox.Show(filePath);
+            return filePath;
+        }
+
+        private void bt_ChooseFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                tb_Folder.Text = folderBrowser.SelectedPath;
+                str_FolderDes = folderBrowser.SelectedPath;
+            }
+        }
+
+        private void bt_Export_Click(object sender, EventArgs e)
+        {
+            lb_caution.ForeColor = Color.DarkRed;
+
+            if (tb_FileName.Text == "")
+            {
+                lb_caution.Text = "Vui lòng chọn đường dẫn cho file cần cắt!";
+                return;
+            }
+
+            if (tb_Folder.Text == "")
+            {
+                lb_caution.Text = "Vui lòng chọn đường dẫn cho thư mục cần lưu file!";
+                return;
+            }
+
+            if (tb__CellSize.Text == "" || tb__CellSize.Text == "0")
+            {
+                lb_caution.Text = "Vui lòng nhập kích thước cell cần cắt!";
+                return;
+            }
+
+            if (tb_numRows.Text == "" || tb_numRows.Text == "0")
+            {
+                lb_caution.Text = "Vui lòng nhập số lượng dòng cần xuất ra!";
+                return;
+
+            }
+
+            Bitmap mainImage = new Bitmap(str_ImageSources);
+            var big_image_width = mainImage.Width;
+            var big_image_height = mainImage.Height;
+
+            int cell_size;
+            int num_rows;
+            Int32.TryParse(tb__CellSize.Text, out cell_size);
+            Int32.TryParse(tb_numRows.Text, out num_rows);
+
+            var big_image_columns = big_image_width / cell_size + 1;
+            var big_image_rows = big_image_height / cell_size + 1;
+
+            List<Cell> listCell = new List<Cell>();
+            int count_num = 1;
+            int cellTotal = 0;
+
+            Bitmap outputImage;
+
+            //Tạo file text cho grid map
+            //Nhiệm vụ ưu tiên là ghi lên file
+            // CreateFileText(str_FolderDes + "\\" + "tile_map.txt");
+            try
+            {
+                File.WriteAllText(str_FolderDes + "\\" + "tile_map.txt", String.Empty);
+                FileStream fileStream = new FileStream(str_FolderDes + "\\" + "tile_map.txt", FileMode.OpenOrCreate, FileAccess.Write);
+                StreamWriter writer = new StreamWriter(fileStream);
+            
+                lb_caution.ForeColor = Color.Aqua;
+
+                bt_Export.Enabled = false;
+            
+
+                for (int i = 0; i < big_image_rows; i++)
+                {
+                    if (i != 0)
+                    {
+                    writer.Write("\n");
+                    }
+                    for (int j = 0; j < big_image_columns; j++)
+                    {
+                        Rectangle section = new Rectangle(new Point(i * cell_size, j * cell_size), new Size(cell_size, cell_size));
+                        Cell cell = new Cell();
+                        cell.cell_Bitmap = CropImage(mainImage, section);
+                        cell.ID = count_num;
+                        cell.count = 1;
+
+                        bool similar = false;
+                        if (cellTotal == 0)
+                        {
+                            listCell.Add(cell);
+                            cellTotal += 1;
+                        }
+                        else
+                        {
+                            for (int k = 0; k < listCell.Count; k++)
+                            {
+                                if (CompareImage(cell.cell_Bitmap, listCell[k].cell_Bitmap))
+                                {
+                                    writer.Write(listCell[k].ID.ToString() + " ");
+                                    //File.WriteAllText(str_FolderDes + "\\" + "tile_map.txt", listCell[k].ID.ToString());
+                                    similar = true;
+                                    //MessageBox.Show("Trung roi");
+                                    Console.WriteLine(listCell[k].ID.ToString());
+                                    break;
+                                }
+                            }
+
+                            if (similar)
+                            {
+                                cellTotal += 1;
+                                lb_caution.Text = "Đang ghi file... (@_@) " + (((float)cellTotal / (big_image_columns * big_image_rows)) * 100) + "%";
+                                lb_caution.Refresh();
+                                continue;
+                            }
+                            else
+                            {
+                                writer.Write(cell.ID.ToString() + " ");
+                                //MessageBox.Show(cell.ID.ToString());
+                                //File.WriteAllText(str_FolderDes + "\\" + "tile_map.txt", cell.ID.ToString());
+                                listCell.Add(cell);
+                                count_num += 1;
+                                cellTotal += 1;
+                                Console.WriteLine(count_num.ToString());
+                                lb_caution.Text = "Đang ghi file... (@_@) " + (((float)cellTotal / (big_image_columns * big_image_rows)) * 100) + "%";
+                                lb_caution.Refresh();
+                                Console.WriteLine("****************************" + cellTotal + (big_image_columns * big_image_rows));
+                            }
+                        }
+                    }
+                }
+
+                //Xóa bộ nhớ đệm của file text
+                writer.Flush();
+                //Đóng ghi file
+                writer.Close();
+                //////////////////////////////////
+                ///Nhiệm vụ tạo sprite tile map
+                //Dán hình vào bitmap lớn///////
+                //Định nghĩa cột hàng của tile map
+                var grid_columns = cellTotal / num_rows;
+                var grid_rows = num_rows;
+                //Tạo bitmap thể hiện cũa tileSet
+                Bitmap map_bitmap = new Bitmap(cell_size * grid_columns, cell_size * grid_rows);
+                Graphics g = Graphics.FromImage(map_bitmap);
+                //vòng lặp thứ hai ghi ảnh bitmap từng cell lên bitmap lớn
+                var count_nember = 0;
+                for(int i = 0; i < grid_rows; i++)
+                    for (int j = 0; j  < grid_columns; j++)
+                    {
+                        if (count_nember >= listCell.Count)
+                            break;
+
+                        g.DrawImage(listCell[count_nember].cell_Bitmap, new Point(j * cell_size, i * cell_size));
+                        lb_caution.Text = "Đang vẽ hình... (!_!) " + (((float)count_nember / listCell.Count) * 100) + "%";
+                        count_nember += 1;
+                    }
+
+                map_bitmap.Save(str_FolderDes + "\\" + "tile_map.png");
+                //Bỏ qua các sự kiện khi bt_Export disable;
+                Application.DoEvents();
+                bt_Export.Enabled = true;
+                lb_caution.Text = "Xong!";
+            }
+            catch
+            {
+                lb_caution.ForeColor = Color.DarkRed;
+                lb_caution.Text = "Có lỗi xảy ra! Kiểm tra lại input. Lưu ý: Không được chọn thư mục đích trong ổ hệ thống! Không nhập kích thước cell lớn hơn ảnh hay số dòng không hợp lệ!";
+            }
+        }
+
+        //So sánh hai ảnh
+        private bool CompareImage(Bitmap bmp1, Bitmap bmp2)
+        {
+            string img1, img2;
+            
+            if (bmp1.Width == bmp2.Width && bmp1.Height == bmp2.Height)
+            {
+                for (int i = 0; i < bmp1.Width; i++)
+                    for (int j = 0; j < bmp1.Height; j++)
+                    {
+                        img1 = bmp1.GetPixel(i, j).ToString();
+                        img2 = bmp2.GetPixel(i, j).ToString();
+                        if (img1 != img2) return false;
+                    }
+            }
+            return true;
+        }
+
+
+        //Hàm cắt ảnh
+        private Bitmap CropImage(Bitmap src, Rectangle section)
+        {
+            var bitmap = new Bitmap(section.Width, section.Height);
+            using (var g = Graphics.FromImage(bitmap))
+            {
+                g.DrawImage(src, 0, 0, section, GraphicsUnit.Pixel);
+                return bitmap;
+            }
+        }
+
+        private void CreateFileText(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+                
+            }
+
+            //Create new file
+            File.Create(fileName);
+        }
+        
+        private void playMusicBg()
+        {
+            try
+            {
+                SoundPlayer sound = new SoundPlayer();
+                sound.SoundLocation = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "..\\.." + "\\closetothesun.wav"));
+                Console.WriteLine(Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "..\\.." + "\\closetothesun.wav")));
+                Console.WriteLine(Environment.CurrentDirectory);
+                sound.PlayLooping();
+            }
+            catch { }
+        }         
+    }
+}
