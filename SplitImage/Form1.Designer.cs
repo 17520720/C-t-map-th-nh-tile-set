cdf58f9e79cba106fcf3602ec71982b77082b1a0
17520720 <50265171+17520720@users.noreply.github.com>
2019-11-28 12:21:52 +0700

    Commit sương sương

--- SplitImage/Form1.Designer.cs
+++ SplitImage/Form1.Designer.cs
@@ -177,6 +177,7 @@
             // 
             // form_SplitImage
             // 
+            this.AcceptButton = this.bt_Export;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
